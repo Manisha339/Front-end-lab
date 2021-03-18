3f4ce51209903dcf38c26efdf60e0343d91fd992
mallu <mallu@HSC-5YQM6Y2>
2021-03-18 10:49:49 +0530

    Datatypes

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,19 @@
+var name1="gnanesh";
+console.log(name1,typeof name1);
+ var age=20;
+ console.log(age,typeof age);
+ var cars =["BMW","AUDI","KIA"];
+ console.log(cars, typeof cars);
+
+ var condition = true;
+ console.log(condition, typeof condition);
+ var obj = { name:"mani", lastname:"Allu"}
+ console.log(obj, typeof obj);
+ var desg=null;
+ console.log(desg, typeof desg);
+ var deg;
+ console.log(deg, typeof deg);
+ var fun= function(){
+     console.log("this is function");
+ }
+ console.log(fun,typeof fun);
